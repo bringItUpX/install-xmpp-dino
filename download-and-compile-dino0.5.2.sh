e7650269568b1a447def8438d8b869a1b8e3a0eb
bringItUpX <41493891+bringItUpX@users.noreply.github.com>
2025-12-22 12:31:06 +0100

    add dino 0.5.2 build script
    the script installs dependencies for dino 0.5.2 and uses meson to compile dino on ubuntu 24.04

---
+++ download-and-compile-dino0.5.2.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+sudo apt install ninja-build valac gettext libgee-0.8-dev libsqlite3-dev libgtk-4-dev libadwaita-1-0 libgpgme-dev libsoup2.4-dev libgcrypt20-dev libqrencode-dev libgstreamer1.0-dev libgstreamer-plugins-base1.0-dev libwebrtc-audio-processing-dev libsrtp2-dev libnice-dev glib-networking gstreamer1.0-plugins-good libadwaita-1-dev
+sudo apt install meson-1.5 libomemo-c-dev
+git clone https://github.com/dino/dino.git
+cd dino
+meson setup build -Dplugin-rtp-h264=enabled -Dplugin-rtp-vaapi=enabled -Dplugin-rtp-vp9=enabled -Dplugin-rtp-webrtc-audio-processing=disabled
+meson compile -C build
+
+echo "If the above ends with success, please start the following file: build/main/dino"
