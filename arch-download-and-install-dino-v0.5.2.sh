83d457490a99a57d7e26a3735b000cdb9b7609d2
coffeetux <coffeetux-socm@mailbox.org>
2026-05-10 11:41:52 +0200

    replaced custom installation with meson install to fix bug that encryption was not working correctly

--- arch-download-and-install-dino-v0.5.2.sh
+++ arch-download-and-install-dino-v0.5.2.sh
@@ -1,30 +1,14 @@
 #!/bin/bash
 
-# this script will installdino in $DINO_TARGET_DIR
+# this script will install dino
 
 # dino build directory
-DINO_TARGET_DIR="/opt/dino"
 DINO_BUILD_DIR_NAME="dino"
 
 # dependencies for dino
 DEPS="webrtc-audio-processing libomemo-c"
 # dependencies for building dino from source
 BUILD_ENV="base-devel gcc meson gettext glib2 gtk4 icu libgee qrencode ninja sqlite vala gpgme libgcrypt libsrtp glib-networking libnice libsoup gst-plugins-good gst-plugins-bad gst-plugins-ugly"
-
-## functions
-
-## returns false (0) if anything other then gnome desktop env is used
-## else true (1)
-## thanks to: https://github.com/alexeevdv/dename
-function isGnome()
-{
-    ps -e | grep -E '^.* gnome-session$' > /dev/null
-    if [ $? -ne 0 ];
-        then
-        return 0
-    fi
-    return 1
-}
 
 printf "Install arch-based dependencies to install dino \n"
 
@@ -37,29 +21,18 @@ git clone https://github.com/dino/dino.git $DINO_BUILD_DIR_NAME >> compile-dino.
 cd $DINO_BUILD_DIR_NAME
 # prepare the build process
 printf "Prepapre the build\n"
-meson setup build \
- -Dplugin-rtp-h264=enabled -Dplugin-rtp-vaapi=enabled -Dplugin-rtp-vp9=disabled -Dplugin-rtp-msdk=enabled -Dplugin-rtp-webrtc-audio-processing=enabled \
-    --wipe >> compile-dino.log 2>&1
+meson setup --reconfigure build \
+ -Dplugin-rtp-h264=enabled -Dplugin-rtp-vaapi=enabled -Dplugin-rtp-vp9=enabled -Dplugin-rtp-msdk=enabled -Dplugin-rtp-webrtc-audio-processing=enabled \
+ --wipe >> compile-dino.log 2>&1
 # actual build dino
 printf "build dino from source\n"
 meson compile -C build >> compile-dino.log 2>&1
 
 # copy desktop file and icon to the system cache
-printf "copy desktop file and icons to system, to make dino available in the desktop environment\n"
-sudo cp main/data/im.dino.Dino.desktop /usr/share/applications/
-sudo cp main/data/icons/scalable/apps/im.dino.Dino.svg /usr/share/icons/hicolor/scalable/apps/
+printf "install dino \n"
 
-# create install diectory
-sudo mkdir -p $DINO_TARGET_DIR
-# copy bin/run files into the target dir 
-sudo cp -r build/* $DINO_TARGET_DIR/
-# make it available in the path
-sudo ln -sf $DINO_TARGET_DIR/main/dino /usr/bin/dino 
+sudo meson install -C build --no-rebuild >> compile-dino.log 2>&1
+echo '/usr/local/lib' | sudo tee -a /etc/ld.so.conf.d/local.conf
+sudo ldconfig
 
-# for gnome desktop update the icon cache and desktop database
-# to make dino available in the application overview and search
-if isGnome; then
-    sudo update-desktop-database /usr/share/applications/
-    sudo gtk-update-icon-cache /usr/share/icons/hicolor/ >> compile-dino.log 2>&1
-fi
 printf "Finished installing dino\n"
